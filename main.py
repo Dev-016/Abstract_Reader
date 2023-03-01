db79a659cf64bdea8d91cbf18296db07884386e9
b9021147 <b9021147@ncl.ac.uk>
2023-03-01 21:23:20 +0000

    Rewrote extracting columns function

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+"""
 # Author: Lee Taylor
+The purpose of this project is to speed up a bibliographic review of literature.
+Please the view the README.md for more information.
+"""
 import os
 import openai
 import api_key
@@ -8,29 +12,33 @@ import pandas as pd
 # Load your API key from an environment variable or secret management service
 openai.api_key = api_key.API_KEY
 
-
-'''
-The purpose of this project is to speed up a bibliographic review of literature. 
-Please the view the README.md for more information.
-'''
+# Define excel file to parse
+fd = "Excels/Lexicon_Construction.xlsx"
 
 
-def extract_ta_pairs():
-    """
-    This function reads the .xlsx into a dataframe (DF).
-    The paper titles and abstracts are extracted from the DF into
-     a list of tuple pairs.
-    """
-    df = pd.read_excel("Excels/Lexicon_Construction.xlsx")
-    doc_title = df['Document Title'].tolist()
-    abstracts = df['Abstract'].tolist()
-    return [(t, a) for t, a in zip(doc_title, abstracts)]
+def extract_cols(col_names):
+    df = pd.read_excel(fd)
+    cols, extracted = [], []
+    for col in col_names:
+        cols.append(df[col].tolist())
+    for i in range(len(cols[0])):
+        extracted.append([arr[i] for arr in cols])
+    # print(extracted)
+    return extracted
 
 
-def create_prompt(title, abstract):
-    user_topic = f'generating or extracting a collection of related words'
+def create_prompt(title, abstract, r_strength=1, user_topic=f'generating or extracting a collection of related words'):
+    if 1 > r_strength > 2:
+        raise ValueError('Parameter r_strength should equal 1 or 2.')
+    r_strength_str = ''
+    # Determine the strength of the relationship between the abstract and user topic
+    if r_strength == 1:
+        r_strength_str = 'Describes, includes or relates to '
+    elif r_strength == 2:
+        r_strength_str = 'Is directly related to '
+    # Form and return prompt as a string
     return f"Determine whether this abstract '{abstract}' from the paper '{title}'." \
-           f" Describes, includes or relates to '{user_topic}'," \
+           f" {r_strength_str} '{user_topic}'," \
            f" you must start your answer with the word 'yes' or 'no' and then your detailed reasoning.'"
 
 
@@ -48,22 +56,22 @@ def response_text(response):
 
 def print_fio(prompt, res):
     print(f"[BEGIN]\n"
-          f"Me: {prompt}\n"
+          f"Me: {prompt}\n\n"
           f"text-davinci-003: {response_text(res).strip()}\n"
           f"[END]\n")
 
 
 if __name__ == '__main__':
-    # Test Response
-    # prompt_ = "What is 2+2?"
-    # res_ = gen_response(prompt_)
-    # print_fio(prompt_, res_)
+    # Extract columns into lists
+    cols_ = extract_cols(['Document Title', 'Abstract', 'PDF Link'])
 
-    # Title-Abstract pairs
-    pairs = extract_ta_pairs()
+    # Define user-topic
+    user_topic_ = f'generating or extracting a collection of related words'
 
     # Generate response for each pair
-    for title, abstract in pairs[80:90]:
-        prompt_ = create_prompt(title, abstract)
+    for i, v in enumerate(cols_):
+        title_, abstract_, pdf_link = v
+        prompt_ = create_prompt(title_, abstract_, 1, user_topic_)
         res_ = gen_response(prompt_)
         print_fio(prompt_, res_)
+        cols_.append(response_text(res_).strip().split()[0])
--- tests.py
+++ tests.py
@@ -2,11 +2,15 @@ from main import *
 
 
 def test_response():
-    prompt_ = "What is 2+2?"
-    res = gen_response(prompt_)
+    prompt = "What is 2+2?"
+    res = gen_response(prompt)
     print(f">>> BEGIN: \n"
-          f"Me: {prompt_}\n"
+          f"Me: {prompt}\n"
           f"text-davinci-003: {response_text(res).strip()}")
+
+
+def test_extract_cols():
+    extract_cols(['Document Title', 'Abstract', 'PDF Link'])
 
 
 if __name__ == '__main__':
