2082ce7746a114383efc3757a008a91b1111bd76
Lee <lees_taylor_18@yahoo.com>
2023-03-02 01:16:11 +0000

    Modified + untested main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ Please the view the README.md for more information.
 """
 import os
 import openai
-import api_key
+import api_key  # Create a file called
 import pandas as pd
+import csv
 
 
 # Load your API key from an environment variable or secret management service
@@ -39,7 +40,8 @@ def create_prompt(title, abstract, r_strength=1, user_topic=f'generating or extr
     # Form and return prompt as a string
     return f"Determine whether this abstract '{abstract}' from the paper '{title}'." \
            f" {r_strength_str} '{user_topic}'," \
-           f" you must start your answer with the word 'yes' or 'no' and then your detailed reasoning.'"
+           f" you must start your answer with the word 'yes' or 'no' with a fullstop " \
+           f"and then your detailed reasoning.'"
 
 
 def gen_response(prompt):
@@ -61,6 +63,12 @@ def print_fio(prompt, res):
           f"[END]\n")
 
 
+def write_to_csv(filename, data):
+    with open(filename, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+
+
 if __name__ == '__main__':
     # Extract columns into lists
     cols_ = extract_cols(['Document Title', 'Abstract', 'PDF Link'])
@@ -68,13 +76,28 @@ if __name__ == '__main__':
     # Define user-topic
     user_topic_ = f'generating or extracting a collection of related words'
 
+    # Storage for .csv writer
+    data_r1, data_r2 = [], []
+
     # Generate response for each pair
     for i, v in enumerate(cols_):
+        # Extract: title, abstract, ...
         title_, abstract_, pdf_link = v
-        prompt_ = create_prompt(title_, abstract_, 1, user_topic_)
-        res_ = gen_response(prompt_)
-        print_fio(prompt_, res_)
-        cols_.append(response_text(res_).strip().split()[0])
+        # Create prompt
+        prompt_1 = create_prompt(title_, abstract_, 1, user_topic_)
+        prompt_2 = create_prompt(title_, abstract_, 2, user_topic_)
+        # Generate response
+        res_1 = gen_response(prompt_1)
+        res_2 = gen_response(prompt_2)
+        # Out response
+        # print_fio(prompt_, res_)
+        # Update storage for later to write to .csv
+        data_r1.append([response_text(res_1).strip().split()[0], title_, abstract_, pdf_link])
+        data_r1.append([response_text(res_2).strip().split()[0], title_, abstract_, pdf_link])
 
     # Todo: Generate a .csv file with the first column being the 'yes' or 'no'
     #  from the text-davinci-003 model response
+
+    # Write to .csv file
+    write_to_csv('output_r1.csv', data)
+
